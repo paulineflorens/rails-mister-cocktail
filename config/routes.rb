c34ba502081a6be96c7c1826f332e11a90423b07
Pauline Florens <pauflorens@gmail.com>
2019-05-23 17:08:07 +0200

    without css

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: "cocktails#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :doses, only: [:create, :new]
+  end
+  resources :doses, only: [:destroy]
 end
